0cd92e76512805ccc3bb04bcdb1f48524a4c50c0
Marshall Abrams <marshall@logical.net>
2024-02-27 15:08:26 -0600

    Cleanup, added attempt, got new version from Lean zulip

--- lean/Depniche.lean
+++ lean/Depniche.lean
@@ -1,3 +1,41 @@
 -- This module serves as the root of the `Depniche` library.
 -- Import modules here that should be built as part of the library.
-import «Depniche».Basic
+-- import «Depniche».Basic
+-- import «Init».Prelude
+
+
+@[match_pattern] -- added because of error message on inNiche, but don't understand
+inductive Niche (k : Nat) where
+  | organism : Nat → Niche k
+deriving Repr
+
+
+-- organism-level niche incrementation: increment an organism's niche
+def incOrganism {k : Nat} (o : Niche k) : Niche k.succ :=
+  match o with
+  | Niche.organism k => Niche.organism k.succ -- why can't I use succ btw?
+
+-- Kyle Miller says it can't work because you can't pattern match on types
+-- because they're erased.
+-- from https://leanprover.zulipchat.com/#narrow/stream/113489-new-members/topic/Construct.20type.20from.20index.20in.20another.20type/near/423694347
+/-
+/-- Generate a new niche from an old niche, incrementing the index. -/
+def incNiche {k : Nat} (n : Type) : Type :=
+  match n with
+  | (Niche k) => Niche k.succ 
+-/
+
+
+#check Niche
+#check (Niche 4)
+
+-- attribute [match_pattern] Niche 
+-- attribute [match_pattern] Niche k
+
+-- Here is another idea.  I can't match on a type, but I can match
+-- on an organism, and it gets to create the niche:
+def org2niche {k : Nat} (o : Niche k) : Type :=
+  match o with
+  | Niche.organism k => Niche k.succ
+
+#check org2niche (Niche.organism 5)
--- lean/Main.lean
+++ lean/Main.lean
@@ -1,3 +1,4 @@
+import «Depniche».Basic
 -- import «Depniche»
 
 def yo : String := " this"
@@ -5,40 +6,6 @@ def yo : String := " this"
 def f (x : Nat) : Nat := x + 1
 
 def f5 : Nat := f 5
-
--- inductive Niche (k : Nat) where
---   | user : Nat → Niche k
-
-@[match_pattern] -- ??
-inductive Niche (k : Nat) where
-  | user : Nat → Niche k
-deriving Repr
-
-def organism : Niche 5 := Niche.user 5
-def niche : Type := Niche 5
-
-
--- organism-level niche incrementation: increment an organism's niche
-def incOrganism (o : Niche k) : Niche (1 + k) :=
-  match o with
-  | Niche.user k => Niche.user (1 + k)
-
-#eval organism
-#eval incOrganism organism
-
-#check Niche
-#check (Niche 4)
-
--- cf. https://proofassistants.stackexchange.com/questions/2438/why-can-addition-be-used-in-pattern-matching-nats-but-not-multiplication
--- attribute [match_pattern] Niche 
--- attribute [match_pattern] Niche k
-
--- Generate a new niche from an old niche, incrementing the index.
--- Doesn't work.
-def incNiche {k : Nat} (n : Type) : Type :=
-  match n with
-  | (Niche k) => Niche (1 + k)
-
 
 
 def main : IO Unit :=
