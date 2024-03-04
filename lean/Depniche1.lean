ff65c4061ff113d0aea3f5cd8de496372ac15bb3
mabrams <mabrams@uab.edu>
2024-03-04 11:52:27 -0600

    New partially working version, reimplementation of Depniche.lean

--- lean/Depniche1.lean
+++ lean/Depniche1.lean
@@ -1,17 +1,61 @@
-inductive Vect (α  : Type u) : Nat → Type u where
-  | nil : Vect α 0
-  | cons : α → Vect α n → Vect α (n + 1)
+
+-- @[match_pattern] -- added because of error message on inNiche, but don't understand
+
+inductive Niche : (k : Nat) → Type where
+  | user : (k : Nat) → Niche k
 deriving Repr
 
--- note example is a keyword
-example : (Vect String 0)  := Vect.nil
+#check Niche
+#check (Niche)
+#check Niche 5
+#check (Niche 5)
 
-example : Vect String 2 := Vect.cons "a" (Vect.cons "b" Vect.nil)
+def o : Niche 5 := Niche.user 5
+#check o
+#eval o
 
-def vs : Vect String 2 := Vect.cons "a" (Vect.cons "b" Vect.nil)
-#check vs
-#eval vs
+def o' := Niche.user 5
+#check o'
+#eval o'
 
-inductive Niche (α : Nat) : Type where
-  | user : k → Niche k
-deriving Repr
+
+-- For this alternative syntax, see https://lean-lang.org/functional_programming_in_lean/getting-to-know/conveniences.html
+def incOrganism : (o : Niche k) → (Niche k.succ)
+  | (Niche.user k) => Niche.user k.succ
+
+
+def oplus : (Niche 6) := incOrganism o
+#check oplus
+#eval oplus
+
+
+def incNicheSilly (n : Type) (k : Nat) : Type :=
+  Niche k.succ
+  
+ -- As I was told in the Zulip chat, this won't work:
+/--
+def incNiche {k : Nat} (n : Type) : Type :=
+  match n with
+  | (Niche k) => Niche k.succ
+--/
+
+-- Though this works.  But note that it doesn't depend directly
+-- on the existence of a Niche k. Otoh, if the niche user exists,
+-- then the Niche must exist.  (Note use of alternate syntax see above.)
+def org2niche : (o : Niche k) → Type
+  | Niche.user k => Niche k.succ
+
+#check org2niche
+#check (org2niche)
+
+def osNewNiche := org2niche o
+#check osNewNiche
+-- #eval osNewNiche
+
+
+
+
+-- btw I never got this syntax working:
+-- inductive Niche (k : Nat) : Type where
+-- It turns out that copying Idris and Agda as closely as possible
+-- makes it work as I intend.
