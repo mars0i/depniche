c01f8a9c3bdd666cac394b39212e7c2cec62baa6
Marshall Abrams <marshall@logical.net>
2024-03-04 23:49:36 -0600

    Tried something new ....

---
+++ lean/Depniche2.lean
@@ -0,0 +1,64 @@
+-- btw I never got this syntax working:
+-- inductive Niche (k : Nat) : Type where
+-- It turns out that copying Idris and Agda as closely as possible
+-- makes it work as I intend.
+
+-- @[match_pattern] -- added because of error message on inNiche, but don't understand
+
+inductive Niche : (k : Nat) → Type where
+  | user : (k : Nat) → Niche k
+deriving Repr
+
+#check Niche
+#check (Niche)
+#check Niche 5
+#check (Niche 5)
+
+def o : Niche 5 := Niche.user 5
+#check o
+#eval o
+
+def o' := Niche.user 5
+#check o'
+#eval o'
+
+
+-- For this alternative syntax, see https://lean-lang.org/functional_programming_in_lean/getting-to-know/conveniences.html
+def incOrganism : (o : Niche k) → (Niche k.succ)
+  | (Niche.user k) => Niche.user k.succ
+
+
+def oplus : (Niche 6) := incOrganism o
+#check oplus
+#eval oplus
+
+
+def incNicheSilly (n : Type) (k : Nat) : Type :=
+  Niche k.succ
+  
+ -- As I was told in the Zulip chat, this won't work:
+/--
+def incNiche {k : Nat} (n : Type) : Type :=
+  match n with
+  | (Niche k) => Niche k.succ
+--/
+
+-- Though the following works.  But note that it doesn't depend directly
+-- on the existence of a Niche k. Otoh, if the niche user exists,
+-- then the Niche exists.  (Note use of alternate syntax see above.)
+def org2niche : (o : Niche k) → Type
+  | Niche.user k => Niche k.succ
+
+#check org2niche
+#check (org2niche)
+
+def osNewNiche : Type := org2niche o
+#check osNewNiche
+-- #eval osNewNiche
+
+
+-- What's needed to modify a Niche is to get access to its parameter(s),
+-- i.e. index(es).  What if we use dependent pairs?
+-- No, Lean doesn't like this:
+-- def nichepair (k : Nat) : ⟨k, Type⟩ := Sigma.mk k (Niche k)
+  
