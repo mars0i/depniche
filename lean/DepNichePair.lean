f11e27286cc372ddb98136e3311c254f467c5dad
Marshall Abrams <marshall@logical.net>
2024-03-13 13:41:32 -0500

    Added notes on syntax of dep pairs vs regular pairs

--- lean/DepNichePair.lean
+++ lean/DepNichePair.lean
@@ -93,7 +93,8 @@ variable gets rid of unused-variable warnings.
 def mkNichePairCrossAngle    (k : ℕ) : (_ : ℕ) × Type  := ⟨k, Niche k⟩
 def mkNichePairSigmaMk (k : ℕ) : (Σ _ : ℕ, Type) := Sigma.mk k (Niche k)
 def mkNichePairNoUnicode (k : Nat) : (Sigma (fun _ : Nat => Type)) := Sigma.mk k (Niche k)
-def mkNichePair := mkNichePairNoUnicode -- chose one of the functions
+def mkNichePair := mkNichePairCrossAngle
+-- def mkNichePair := mkNichePairNoUnicode -- chose one of the functions
 
 -- We can define an abbreviation for that Sigma type, though it doesn't
 -- work to use it with CoeSort, apparently.
@@ -193,3 +194,28 @@ def nichevect := Vector.cons Niche4 $ Vector.cons Niche6 Vector.nil  -- It's bet
 -- #eval Vector.map incNiche nichevect -- Lean doesn't know how to display this.
 #eval Vector.map (fun p => p.fst) $ Vector.map incNiche nichevect
 
+--------------------------------------------
+-- Notes on regular pairs vs. dependent pairs in Lean:
+
+-- We can create ordered pairs either using parentheses or angle brackets:
+def pair1 : Nat × String := (1, "2")
+def pair2 : Nat × String := ⟨1, "2"⟩ 
+#check pair1
+#check pair2
+#eval pair1
+#eval pair2
+
+-- def badpair1 : Nat × String := Sigma.mk 1 "2"
+
+def pair3 : (Σ n : Nat, String) := ⟨1, "2"⟩
+#check pair3
+#eval pair3
+-- def pair4 : (Σ n : Nat, String) := (1, "2") -- but you can't use parentheses for Sigma types.
+
+-- Merely adding/removing a name for the instance of the first type changes the type.
+def pair4 : (k : Nat) × String := ⟨1, "2"⟩
+#check pair4
+#eval pair4
+-- That's a potential source of bugs, and is a reason to avoid using angle brackets rather than Sigma.mk.
+-- Otoh the subtle differences in notation are built into how these types and values are reported,
+-- and using the cross and angle brackets vs. parens reflects those representations.
