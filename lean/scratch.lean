22c97727185a352bef8e376637b8ca565d53b3e0
mabrams <mabrams@uab.edu>
2024-03-13 16:59:07 -0500

    Added some experiments

--- lean/scratch.lean
+++ lean/scratch.lean
@@ -4,10 +4,6 @@ inductive Niche : (k : ℕ) → Type where
   | user : (k : ℕ) → Niche k
 deriving Repr
 
-def Niche.type (_ : Niche k) : Type := Niche k
-def Niche.alsotype : (Niche k) → Type := Niche
-def Niche.fourtytwo : Nat := 42
-
 def n := Niche 4
 #check n
 
@@ -15,12 +11,42 @@ def u := Niche.user 4
 #check u
 #eval u
 
+def Niche.type (_ : Niche k) : Type := Niche k
+def Niche.alsotype : (Niche k) → Type := Niche
+
 #check u.type
 #check u.alsotype
--- #check u.fourtytwo -- error. Why? Maybe only if it's a fn is it attached to the instance.
 
-#check Niche.fourtytwo
-#eval Niche.fourtytwo
 -- sigs are same except for one var name:
 #check Niche.type
 #check Niche.alsotype
+
+def Niche.fourtytwo : Nat := 42
+#check Niche.fourtytwo
+#eval Niche.fourtytwo
+-- #check u.fourtytwo -- error. Why? Maybe only if it's a fn is it attached to the instance.
+
+
+structure Nicz (typeidx : Nat) where
+  instanceidx : Nat
+  deriving Repr
+
+-- External constructor that forces the type index and field to be the same:
+def mkNicz (k : Nat) : Nicz k := Nicz.mk k
+#check mkNicz
+#check (mkNicz)
+def ncz := mkNicz 5
+#check ncz
+#eval ncz
+-- But you can still do this:
+def nczbad : Nicz 5 := {ncz with instanceidx := 15}
+-- or even:
+def niczarefree : Nicz 42 := {ncz with instanceidx := 15}
+-- So one must not do that.
+
+-- I'm not even sure that you can prove that the the two indexes are the
+-- same, given that there's no typecase, and the field value isn't 
+-- represented in the type.
+-- So it's a job for the universe pattern, I guess, but I'm sceptical
+-- still.
+
